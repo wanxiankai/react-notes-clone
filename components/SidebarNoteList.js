99acd60fa15b0ecd7088b8030511c8ae3b5955a3
wanxiankai <wanxiankai@126.com>
2024-01-04 11:03:59 +0800

    feat: 修改侧边栏组件

---
+++ components/SidebarNoteList.js
@@ -0,0 +1,23 @@
+export default async function NoteList({ notes }) {
+
+    const arr = Object.entries(notes);
+  
+    if (arr.length == 0) {
+      return <div className="notes-empty">
+        {'No notes created yet!'}
+      </div>
+    }
+  
+    return <ul className="notes-list">
+      {arr.map(([noteId, note]) => {
+      const { title, updateTime } = JSON.parse(note);
+      return <li key={noteId}>
+        <header className="sidebar-note-header">
+          <strong>{title}</strong>
+          <small>{updateTime}</small>
+        </header>
+      </li>
+    })}
+    </ul>
+  }
+  
