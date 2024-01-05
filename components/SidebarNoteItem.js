a3e1aba8f330c0382b84a958f9eb0fcc3129a3a5
wanxiankai <wanxiankai@126.com>
2024-01-05 10:00:02 +0800

    feat: 抽离header组件

--- components/SidebarNoteItem.js
+++ components/SidebarNoteItem.js
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import SidebarNoteItemContent from '@/components/SidebarNoteItemContent';
+import SidebarNoteItemHeader from '@/components/SidebarNoteItemHeader';
 
 export default function SidebarNoteItem({ noteId, note}) {
 
@@ -13,10 +14,7 @@ export default function SidebarNoteItem({ noteId, note}) {
           {content.substring(0, 20) || <i>(No content)</i>}
         </p>
       }>
-      <header className="sidebar-note-header">
-        <strong>{title}</strong>
-        <small>{dayjs(updateTime).format('YYYY-MM-DD hh:mm:ss')}</small>
-      </header>
+      <SidebarNoteItemHeader title={title} updateTime={updateTime} />
     </SidebarNoteItemContent>
   );
 }
