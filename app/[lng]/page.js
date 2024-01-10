d7efb8291723f2f2bc914745cba5702080df1bb2
wanxiankai <wanxiankai@126.com>
2024-01-10 21:01:25 +0800

    feat: 布局和页面获取动态语言设置

--- app/[lng]/page.js
+++ app/[lng]/page.js
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function Home({
+  params: {
+    lng
+  }
+}) {
   return (
     <div className="note--empty-state">
       <span className="note-text--empty-state">
-        Click a note on the left to view something! 🥺
+        Click a {lng} note on the left to view something! 🥺
       </span>
     </div>
   )
