ef21cad5a0ac1abd5970956a9fc5d493a150fafd
wanxiankai <wanxiankai@126.com>
2024-01-10 20:52:26 +0800

    feat: 安装@formatjs/intl-localematcher & negotiator库，初始化middleware处理国际化

---
+++ middleware.js
@@ -0,0 +1,34 @@
+// middleware.js
+import { match } from '@formatjs/intl-localematcher'
+import Negotiator from 'negotiator'
+import { locales, defaultLocale } from '@/config.js'
+
+
+function getLocale(request) { 
+  const headers = { 'accept-language': request.headers.get('accept-language') || '' };
+  const languages = new Negotiator({ headers }).languages();
+
+  return match(languages, locales, defaultLocale)
+ }
+ 
+export function middleware(request) {
+  const { pathname } = request.nextUrl
+  // 判断请求路径中是否已存在语言，已存在语言则跳过
+  const pathnameHasLocale = locales.some(
+    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+  )
+ 
+  if (pathnameHasLocale) return
+
+ 
+  // 获取匹配的 locale
+  const locale = getLocale(request)
+  request.nextUrl.pathname = `/${locale}${pathname}`
+  // 重定向，如 /products 重定向到 /en-US/products
+  return Response.redirect(request.nextUrl)
+}
+ 
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
+
