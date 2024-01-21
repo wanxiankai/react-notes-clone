30a2393b0dd83a4859b2b943a04c99000383e2b6
wanxiankai <wanxiankai@126.com>
2024-01-21 14:30:34 +0800

    feat: 更新导入组件，使用onChange 事件调用上传文件api

--- components/SidebarImport.js
+++ components/SidebarImport.js
@@ -1,15 +1,47 @@
 'use client'
+import React from 'react'
+import { useRouter } from 'next/navigation'
 
-import React, { Suspense } from 'react'
+export default function SidebarImport(){
+    const router = useRouter()
+    const onChange = async (e) => {
+        console.log('onChange', e.target)
+        const fileInput = e.target;
 
-export default function SidebarImport() {
-  return (
-    <form method="post" enctype="multipart/form-data">
-      <div style={{ textAlign: "center" }}>
-        <label for="file" style={{ cursor: 'pointer' }}>Import .md File</label>
-        <input type="file" id="file" name="file" multiple style={{ position : "absolute", clip: "rect(0 0 0 0)" }} />
-      </div>
-    </form>
-  )
+        if(!fileInput.files || fileInput.files.length === 0){
+            console.warn("file list is empty")
+            return;
+        }   
+
+        const file = fileInput.files[0];
+
+        const formData = new FormData();
+        formData.append('file', file)
+
+        try {
+            const response = await fetch('/api/upload', {
+                method: "POST",
+                body: formData,
+            })
+            if(!response.ok) {
+                console.error("333something went wrong")
+                return;
+            }
+            
+            const data = await response.json();
+            router.push(`/note/${data.uid}`)
+        } catch (error) {
+            console.error("4444something went wrong")
+        }
+
+        e.target.type = 'text';
+        e.target.type = 'file';
+    }
+
+    return (
+        <div style={{textAlign: 'center'}}>
+            <label htmlFor='file' style={{cursor:'pointer'}}>Import .md File</label>
+            <input onChange={onChange} type='file' id='file' name='file' style={{position:'absolute', clip:'rect(0,0,0,0)'}} accept='.md' />
+        </div>
+    )
 }
-
